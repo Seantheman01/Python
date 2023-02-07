b6e2e6917235da94d928b40b52626dc7e74f7770
Seantheman01 <sean.v.g@outlook.com>
2023-02-07 15:41:46 +0100

    commit

---
+++ Module_03/Deel_01/robot_arm_basics/robotarm_opdracht5.py
@@ -0,0 +1,15 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 5')
+
+# Jouw python instructies zet je vanaf hier:
+for x in range(7):
+    robotArm.moveRight()
+for x in range(8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
