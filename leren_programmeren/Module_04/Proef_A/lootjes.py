d3e50f8f5bb9a9c3e69ac0781b5024846bd9d039
Seantheman01 <sean.v.g@outlook.com>
2023-02-09 15:02:40 +0100

    committed

--- leren_programmeren/Module_04/Proef_A/lootjes.py
+++ leren_programmeren/Module_04/Proef_A/lootjes.py
@@ -5,10 +5,13 @@ teller = 0
 
 while True:
     naam_invullen = input("Vul een naam in: ")
-    namen.append(naam_invullen)
-    teller += 1
+    if naam_invullen not in namen:
+        namen.append(naam_invullen)
+        teller += 1
+    else:
+        print("Je hebt die naam al!")
+        
     naam_toevoegen = input("Wil je nog een naam toevoegen? Typ ja of nee: ")
-    
     if naam_toevoegen == 'nee' and teller <=2:
         print("Je moet meer namen toevoegen!")
     elif naam_toevoegen == 'nee' and teller >=3:
@@ -16,6 +19,3 @@ while True:
 
 for x in namen:
     print(f"{random.choice(namen)} heeft {random.choice(namen)}")
-    
-if naam_invullen in namen:
-        random.shuffle(namen)
