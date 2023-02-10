34ed8a330a5162f40c8a9359f16ad26eafff9db6
Seantheman01 <sean.v.g@outlook.com>
2023-02-10 10:22:27 +0100

    committed

--- leren_programmeren/Module_05/Deel_01.a/hello.py
+++ leren_programmeren/Module_05/Deel_01.a/hello.py
@@ -2,5 +2,5 @@ def mijn_functie(getal = 1):
     return getal + 1
 
 for x in range(10):
-    print(f"Hello from function town {mijn_functie(1)}")
+    print(f"Hello from function town {mijn_functie(0)}")
     
