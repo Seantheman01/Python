eda825869ce35156191e84fe6863e4f320c91eac
Seantheman01 <sean.v.g@outlook.com>
2023-02-16 13:33:54 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/leeftijden.py
+++ leren_programmeren/Module_05/Deel_01/leeftijden.py
@@ -1,6 +1,6 @@
 def mijn_functie(naam, leeftijd):
-    naam = input("Typ een naam in: ")
-    leeftijd = int(input("Typ een leeftijd in: "))
+    input("Typ een naam in: ")
+    int(input("Typ een leeftijd in: "))
     print(naam + "is" + leeftijd + "jaar")
-    
+
 print(mijn_functie(mijn_functie))
