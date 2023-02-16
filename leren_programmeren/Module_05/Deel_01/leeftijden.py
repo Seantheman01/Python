8cf1b2a1454f20fab913d35d740082cca906a097
Seantheman01 <sean.v.g@outlook.com>
2023-02-16 13:37:59 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/leeftijden.py
+++ leren_programmeren/Module_05/Deel_01/leeftijden.py
@@ -1,6 +1,10 @@
-def mijn_functie(naam, leeftijd):
-    input("Typ een naam in: ")
-    int(input("Typ een leeftijd in: "))
+mijn_lijst = []
+
+def mijn_functie(x: int):
+    naam = input("Typ een naam in: ")
+    mijn_lijst.append(naam)
+    leeftijd = int(input("Typ een leeftijd in: "))
+    mijn_lijst.append(leeftijd)
     print(naam + "is" + leeftijd + "jaar")
 
 print(mijn_functie(mijn_functie))
