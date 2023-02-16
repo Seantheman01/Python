c07fe5cb8daff0276970ac831d75f712d0b9f7c8
Seantheman01 <sean.v.g@outlook.com>
2023-02-16 14:08:53 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/leeftijden.py
+++ leren_programmeren/Module_05/Deel_01/leeftijden.py
@@ -2,6 +2,7 @@ mijn_lijst = []
 
 def mijn_functie(x: int):
     naam = input("Typ een naam in: ")
+    verder = input("Wil je doorgaan? Typ 'stop' als je wilt stoppen: ")
     mijn_lijst.append(naam)
     leeftijd = int(input("Typ een leeftijd in: "))
     mijn_lijst.append(leeftijd)
