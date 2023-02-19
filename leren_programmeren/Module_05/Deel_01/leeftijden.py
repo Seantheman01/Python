5bbf2a78e9708160d962ee7a27c320e68357f4f8
Seantheman01 <sean.v.g@outlook.com>
2023-02-19 22:16:32 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/leeftijden.py
+++ leren_programmeren/Module_05/Deel_01/leeftijden.py
@@ -1,14 +1,14 @@
-nieuwe_lijst = []
+naam_lijst = []
+leeftijd_lijst= []
 
-def mijn_functie(x: int):
-        nieuwe_dictionairy = {}
+def mijn_functie(x):
         naam = input("Typ een naam in: ")
+        naam_lijst.append(naam)
         leeftijd = int(input("Typ een leeftijd in: "))
+        leeftijd_lijst.append(leeftijd)
         verder = input("Toets enter om door te gaan of 'stop' om te printen: ")
-        nieuwe_dictionairy['naam'] = naam
-        nieuwe_dictionairy['leeftijd'] = leeftijd
         if verder != 'stop':
-            return nieuwe_dictionairy
+            print(x)
 
 while True:
     
