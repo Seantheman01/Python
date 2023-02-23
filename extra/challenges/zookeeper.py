53855e6f47cfb1b5d41020b5b1b178b1b20add90
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 10:18:19 +0100

    commit

---
+++ extra/challenges/zookeeper.py
@@ -0,0 +1,8 @@
+def mijn_functie():
+    giraffen_aantal = int(input("Hoeveel giraffen zijn er? "))
+    struisvogels_aantal = int(input("Hoeveel struisvogels zijn er? "))
+    zebras_aantal = int(input("Hoeveel zebra's zijn er? "))
+
+    giraf_poten = 4
+    struisvogel_poten = 2
+    zebra_poten = 4
