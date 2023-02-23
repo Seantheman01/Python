135d2951bfe1a045d9fbcd37371a28f438d9acb7
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 11:45:16 +0100

    commit

--- extra/challenges/zookeeper.py
+++ extra/challenges/zookeeper.py
@@ -1,14 +1,14 @@
 # opdracht 1:
 def mijn_functie():
-    giraffen_aantal = int(input("Hoeveel giraffen zijn er? "))
-    struisvogels_aantal = int(input("Hoeveel struisvogels zijn er? "))
-    zebras_aantal = int(input("Hoeveel zebra's zijn er? "))
+    giraf_aantal = int(input("Hoeveel giraffen zijn er? "))
+    struisvogel_aantal = int(input("Hoeveel struisvogels zijn er? "))
+    zebra_aantal = int(input("Hoeveel zebra's zijn er? "))
 
     GIRAF_POTEN = 4
     STRUISVOGEL_POTEN = 2
     ZEBRA_POTEN = 4
     
-    aantal_poten = giraffen_aantal * GIRAF_POTEN + struisvogels_aantal * STRUISVOGEL_POTEN + zebras_aantal * ZEBRA_POTEN
+    aantal_poten = giraf_aantal * GIRAF_POTEN + struisvogel_aantal * STRUISVOGEL_POTEN + zebra_aantal * ZEBRA_POTEN
     return(aantal_poten)
 
 print(f"Er zijn {mijn_functie()} poten.")
--- extra/voorbeelden/zookeeper_voorbeeld.py
+++ extra/voorbeelden/zookeeper_voorbeeld.py
@@ -1,15 +1,20 @@
-giraffen_aantal = 1
-struisvogels_aantal = 1
-zebras_aantal = 1
+giraf_aantal = 1
+struisvogel_aantal = 1
+zebra_aantal = 1
+
+# giraf_aantal = int(input("Hoeveel giraffen zijn er? "))
+# struisvogel_aantal = int(input("Hoeveel struisvogels zijn er? "))
+# zebra_aantal = int(input("Hoeveel zebra's zijn er? "))
 
 GIRAF_POTEN = 4
 STRUISVOGEL_POTEN = 2
 ZEBRA_POTEN = 4
 
 def mijn_functie(giraffen, struisvogels, zebras):
-    aantal_poten = giraffen_aantal * GIRAF_POTEN + struisvogels_aantal * STRUISVOGEL_POTEN + zebras_aantal * ZEBRA_POTEN
+    aantal_poten = giraf_aantal * GIRAF_POTEN + struisvogel_aantal * STRUISVOGEL_POTEN + zebra_aantal * ZEBRA_POTEN
     return(aantal_poten)
 
 print(mijn_functie())
 # print(mijn_functie(5,3,2)) dit mag
-# print(mijn_functie(5,3,struisvogels_aantal)) dit mag ook
+# print(mijn_functie(5,3,zebra_aantal)) dit mag ook
+# print(giraf_aantal, struisvogel_aantal, zebra_aantal)) en dit ook
