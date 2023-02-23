739c57facee86aa2a753801c8c6b1e09d3e535ae
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 11:40:07 +0100

    committed

--- extra/voorbeelden/zookeeper_voorbeeld.py
+++ extra/voorbeelden/zookeeper_voorbeeld.py
@@ -1,14 +1,13 @@
-GIRAF_POTEN = 4
-STRUISVOGEL_POTEN = 2
-ZEBRA_POTEN = 4
-
 giraffen_aantal = 1
 struisvogels_aantal = 1
 zebras_aantal = 1
 
+GIRAF_POTEN = 4
+STRUISVOGEL_POTEN = 2
+ZEBRA_POTEN = 4
+
 def mijn_functie(giraffen, struisvogels, zebras):
     aantal_poten = giraffen_aantal * GIRAF_POTEN + struisvogels_aantal * STRUISVOGEL_POTEN + zebras_aantal * ZEBRA_POTEN
-    
     return(aantal_poten)
 
 print(mijn_functie())
