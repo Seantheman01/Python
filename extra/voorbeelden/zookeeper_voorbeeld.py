cec791ef038454f465c48b09861a94f09ab0617c
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 12:13:56 +0100

    commit

--- extra/voorbeelden/zookeeper_voorbeeld.py
+++ extra/voorbeelden/zookeeper_voorbeeld.py
@@ -19,29 +19,3 @@ print(mijn_functie())
 # print(mijn_functie(5,3,2)) dit mag
 # print(mijn_functie(5,3,zebra_aantal)) dit mag ook
 # print(giraf_aantal, struisvogel_aantal, zebra_aantal)) en dit ook
-
-
-# -----------------------------------------------------------------
-
-
-# opdracht 2:
-tekst = """En ze stu[re]n [i]ngekleurde prentbriefkaarten van plekken waarvan ze zich niet 
-reali[s]eren dat ze er nooit geweest zijn [a]an ' Iedereen op nummer 22, weer is prachti[g],
-onz[e] kamer is aa[n]gekruisd. Missen jullie. E[t]en[ ]i[s] vettig , maar we hebben een geweldig
-leuk restaurantje gevonden in de achterstraatjes waar ze Heine[ke]n hebben en kaas en uien
-chips en iemand die "Een beetje verliefd" speel[t] op een a[c]cordeon ' en je zit vier dagen vast
-op Schip[h]ol voor je vijfdaagse vliegvakantie met niks anders te eten dan uitgedroogde
-voorverpakte boterhammen..."""
-
-tussen_haakjes = ''
-binnen_haakjes = False
-
-for character in tekst:
-    if binnen_haakjes and character != ']':
-        tussen_haakjes += character
-    elif character == '[':
-        binnen_haakjes = True
-    elif character == ']':
-        binnen_haakjes = False
-        
-print(tussen_haakjes)
