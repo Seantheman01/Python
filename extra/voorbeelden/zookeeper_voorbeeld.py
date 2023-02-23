2e5ce8a39afc64bc65110cc3f1d8645b61978063
Seantheman01 <sean.v.g@outlook.com>
2023-02-23 12:04:12 +0100

    committed

--- extra/voorbeelden/zookeeper_voorbeeld.py
+++ extra/voorbeelden/zookeeper_voorbeeld.py
@@ -1,3 +1,4 @@
+# opdracht 1:
 giraf_aantal = int(input("Hoeveel giraffen zijn er? "))
 struisvogel_aantal = int(input("Hoeveel struisvogels zijn er? "))
 zebra_aantal = int(input("Hoeveel zebra's zijn er? "))
@@ -18,3 +19,22 @@ print(mijn_functie())
 # print(mijn_functie(5,3,2)) dit mag
 # print(mijn_functie(5,3,zebra_aantal)) dit mag ook
 # print(giraf_aantal, struisvogel_aantal, zebra_aantal)) en dit ook
+
+
+# -----------------------------------------------------------------
+
+
+# opdracht 2:
+tekst = """En ze stu[re]n [i]ngekleurde prentbriefkaarten van plekken waarvan ze zich niet 
+reali[s]eren dat ze er nooit geweest zijn [a]an ' Iedereen op nummer 22, weer is prachti[g],
+onz[e] kamer is aa[n]gekruisd. Missen jullie. E[t]en[ ]i[s] vettig , maar we hebben een geweldig
+leuk restaurantje gevonden in de achterstraatjes waar ze Heine[ke]n hebben en kaas en uien
+chips en iemand die "Een beetje verliefd" speel[t] op een a[c]cordeon ' en je zit vier dagen vast
+op Schip[h]ol voor je vijfdaagse vliegvakantie met niks anders te eten dan uitgedroogde
+voorverpakte boterhammen..."""
+
+haakjes = ''
+
+for character in tekst:
+    print(character)
+    
