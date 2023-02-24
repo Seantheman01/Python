5b88a5777cf8fd0c992ed43ddfb9a312ca6554da
Seantheman01 <sean.v.g@outlook.com>
2023-02-24 09:21:54 +0100

    committed

--- extra/oefeningen/spelletje.py
+++ extra/oefeningen/spelletje.py
@@ -2,15 +2,15 @@ def mijn_functie():
     
     aantal_spellen = int(input("Hoeveel spellen wilt u? "))
     
-    SPELLETJE = 24.95
+    PRIJS_SPELLETJE = 24.95
     korting = 0.4
     leveren = 1
     extra = aantal_spellen * 0.25
     
     if aantal_spellen <2:
-        som = aantal_spellen * SPELLETJE * korting + leveren
+        som = aantal_spellen * PRIJS_SPELLETJE * korting + leveren
     elif aantal_spellen >=2:
-        som = aantal_spellen * SPELLETJE * korting + leveren + extra
+        som = aantal_spellen * PRIJS_SPELLETJE * korting + leveren + extra
     
     return som
 
