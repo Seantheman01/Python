518eef6c1e94b73867ffb706b500e6756c5faa77
Seantheman01 <sean.v.g@outlook.com>
2023-03-04 22:17:21 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/fibonacci.py
+++ leren_programmeren/Module_05/Deel_01/fibonacci.py
@@ -1,6 +1,8 @@
 def fibonacci(x):
-    teller = 0
-    
-    while teller <= x:
+    # teller = 0
+    # while teller <= x:
         if x in {0, 1}:
             return x
+        return fibonacci(x - 1) + fibonacci(x - 2)
+    
+    
