7049cbd4ca89b348904d57efc0fcf2b7e98ef369
Seantheman01 <sean.v.g@outlook.com>
2023-03-06 09:10:38 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,1 +1,3 @@
 som = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
+getal1 = int(input("Typ het eerste getal in: "))
+getal2 = int(input("Typ het tweede getal in: "))
