281eab38f9dbee0a61b8420777ead8e89f83f800
Seantheman01 <sean.v.g@outlook.com>
2023-03-06 09:14:33 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,3 +1,5 @@
-som = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
-getal1 = int(input("Typ het eerste getal in: "))
-getal2 = int(input("Typ het tweede getal in: "))
+som1 = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
+n1 = int(input("Typ het eerste getal in: "))
+n2 = int(input("Typ het tweede getal in: "))
+
+som2 = int(input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
