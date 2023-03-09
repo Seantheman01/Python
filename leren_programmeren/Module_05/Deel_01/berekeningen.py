02f845d2bbbf2d1c4e031a37abf83b09886a9014
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 09:31:18 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,7 +1,7 @@
 def addition(number1, number2):
     som1 = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
-    if som1 == ''
-n1 = int(input("Typ het eerste getal in: "))
-n2 = int(input("Typ het tweede getal in: "))
+    if som1 == 'a' or som1 == 'b' or som1 == 'c' or som1 == 'd':
+        n1 = int(input("Typ het eerste getal in: "))
+        n2 = int(input("Typ het tweede getal in: "))
 
-som2 = int(input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
+som2 = int(input(f"Wil je wat met de uitkomst () doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
