2ce03d23197f562b8d4ea7c5fcde7664004c8b9f
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 08:55:15 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,5 +1,4 @@
 def addition(number1, number2):
-    
 
 som1 = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
 n1 = int(input("Typ het eerste getal in: "))
