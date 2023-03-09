95e86e2c30b33b5906a86922c967e33bbb8d9318
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 13:53:35 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -10,10 +10,10 @@ def multiplication(number1, number2):
 def division(number1, number2):
     print(number1 / number2)
     
-addition()
-substraction()
-multiplication()
-division()
+addition(2,1)
+substraction(2,1)
+multiplication(2,1)
+division(2,1)
 
 # choice = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
 # choice = int(input(f"Wil je wat met de uitkomst () doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
