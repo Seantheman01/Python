d499958a8609edcd19ebb7b65ec25b5ccf51674d
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 14:08:00 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -15,7 +15,13 @@ substraction(2,1)
 multiplication(2,1)
 division(2,1)
 
-input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
-
-# choice = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
-# choice = int(input(f"Wil je wat met de uitkomst () doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
+choice = input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
+if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
+    n1 = int(input("Voer het eerste getal in: "))
+    n2 = int(input("Voer het tweede getal in: "))
+elif choice == 'e' or choice == 'f':
+    n1 = int(input("Voer een getal in: "))
+    n2 = 1
+elif choice == 'g' or choice == 'h':
+    n1 = int(input("Voer een getal in: "))
+    n2 = 2
