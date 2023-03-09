0817c31ac1f8beb6214588634a6f6d899e33c446
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 14:54:30 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -16,12 +16,31 @@ multiplication(2,1)
 division(2,1)
 
 choice = input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
+
 if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
     n1 = int(input("Voer het eerste getal in: "))
     n2 = int(input("Voer het tweede getal in: "))
+
 elif choice == 'e' or choice == 'f':
     n1 = int(input("Voer een getal in: "))
     n2 = 1
 elif choice == 'g' or choice == 'h':
     n1 = int(input("Voer een getal in: "))
     n2 = 2
+    
+# if choice == 'a':
+#     n1 + n2
+# elif choice == 'b':
+#     n1 - n2
+# elif choice == 'c':
+#     n1 * n2
+# elif choice == 'd':
+#     n1 / n2
+# elif choice == 'e':
+#     n1 + n2
+# elif choice == 'f':
+#     n1 - n2
+# elif choice == 'g':
+#     n1 * n2
+# elif choice == 'h':
+#     n1 / n2 
