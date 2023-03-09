baa02ca07d807cf8031f374427a4b4867c3acb2f
Seantheman01 <sean.v.g@outlook.com>
2023-03-09 13:47:28 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,5 +1,19 @@
 def addition(number1, number2):
-    choice = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
+    print(number1 + number2)
     
+def substraction(number1, number2):
+    print(number1 - number2)
+    
+def multiplication(number1, number2):
+    print(number1 * number2)
+    
+def division(number1, number2):
+    print(number1 / number2)
+    
+addition()
+substraction()
+multiplication()
+division()
 
-choice = int(input(f"Wil je wat met de uitkomst () doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
+# choice = int(input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren?"))
+# choice = int(input(f"Wil je wat met de uitkomst () doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?"))
