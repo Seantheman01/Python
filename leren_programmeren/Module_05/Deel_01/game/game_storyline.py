2e66273c39fbf02fc0d075bba00ed44dc5d35bf1
Seantheman01 <sean.v.g@outlook.com>
2023-03-10 10:29:08 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/game/game_storyline.py
+++ leren_programmeren/Module_05/Deel_01/game/game_storyline.py
@@ -1,6 +1,6 @@
 storyline = {
     
-"""You are the best trained commander of an army. You have been at war with your enemy for years, and your king is done with it. 
+"""You are the best trained commander of an army. You have been at war with your enemy for many years, and your king is done with it. 
 He sends you and a few other knights out with a letter to end the war.
 Starting your journey, you have to choose 2 paths first: forest or village.
 Choose wich path to go: """,
