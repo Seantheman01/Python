d5ef7ffb5bc7a890f459007e139f02b7a760f15c
Seantheman01 <sean.v.g@outlook.com>
2023-03-10 09:53:19 +0100

    committed

--- leren_programmeren/Module_02/Proef_A/game.py
+++ leren_programmeren/Module_02/Proef_A/game.py
@@ -118,7 +118,7 @@ geld = 100
 naam = input("Before we start, what is your name? ")
 intro = input(f"""
 Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. One of the first paths is harder than the other.
-That is all you need to know now. 
+That is all you need to know for now. 
 Good luck on your journey! Are you ready (just type 'yes or no')? """)
 if intro == 'no':
     print("Then why are you playing this?")
