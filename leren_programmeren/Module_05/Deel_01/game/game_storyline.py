079cb68073e2a75b63182a33b1043101cf834d44
Seantheman01 <sean.v.g@outlook.com>
2023-03-13 13:16:48 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/game/game_storyline.py
+++ leren_programmeren/Module_05/Deel_01/game/game_storyline.py
@@ -1,115 +1,115 @@
 storyline = {
     
-"1": """You are the best trained commander of an army. You have been at war with your enemy for many years, and your king is done with it. 
+"""You are the best trained commander of an army. You have been at war with your enemy for many years, and your king is done with it. 
 He sends you and a few other knights out with a letter to end the war.
 Starting your journey, you have to choose 2 paths first: forest or village.
 Choose wich path to go: """,
 
-"2": """You and your team chose the forest. But in the forest they have to choose another route.
+"""You and your team chose the forest. But in the forest they have to choose another route.
 One leads to the swamp, and the other one to the elf village.
 Choose wich path to go: swamp or elf village? """,
 
-"3": """You enter the elf village, and the elfs greet you politely.    
+"""You enter the elf village, and the elfs greet you politely.    
 They offer you some supplies for a very low price. But you don't really wanna pay.
 What will you do: buy or steal? """,
 
-"4": """You steal the supplies and run. The elfs don't like thiefs. They shoot 5 men in their leg.
+"""You steal the supplies and run. The elfs don't like thiefs. They shoot 5 men in their leg.
 You don't want to leave them, but you don't know if you can save them.
 What will you do: save them (just type 'save') or continue? """,
 
-"5": """You want to save them, but you have to continue.
+"""You want to save them, but you have to continue.
 Eventually you reach the lake, but you need to get across.
 How will you cross the river: swim or use a log (just type 'log')? """,
 
-"6": """You and your team cut down a tree to use the log as a bridge.
+"""You and your team cut down a tree to use the log as a bridge.
 You managed to get across the river using the log. On the other side, there is a cave.
 You and your team enter the cave. After walking in a straight line for a while, 2 paths appear.
 One path is blocked off, and the other is a dangerous looking mine.
 Wich path to take: mine or blocked off area (just type 'blocked')? """,
 
-"7": """You enter the mine, and the path is pretty dangerous.
+"""You enter the mine, and the path is pretty dangerous.
 There are a lot holes and sharp rocks to watch out for. But out of nowhere, a big troll appears.
 What will you do: fight, run or yell at it (just type 'yell')? """,
 
-"8": """You all start yelling at the troll. It gets scared, and runs away.
+"""You all start yelling at the troll. It gets scared, and runs away.
 You continue the path, and come across a wall full off gold ores.
 Will you mine it? (type 'yes' or 'no') """,
 
-"9": """You start running from the troll. But he takes bigger steps so it catches up.
+"""You start running from the troll. But he takes bigger steps so it catches up.
 It punches 6 men, and they don't survive.
 You continue the path, and come across a wall full off gold ores.
 Will you mine it? (type 'yes' or 'no') """,
 
-"10": """You don't have enough money. The guards won't let you in.
+"""You don't have enough money. The guards won't let you in.
 Then one of the guards said: 'you could give up some of your men. If you give 3 men, we will let you in.
 You hesitate, but you have to give up 3 men. After a while, 3 men step forward.
 The guards open the gate and let you go in.
 You talk to the enemy king about peace, and that is the story of how the war ended.
 >>> THE END <<<""",
 
-"11": """You and your team choose the village.
+"""You and your team choose the village.
 You enter the village, but all the villagers went silent.
 You tell them you are no threat, but they still don't really trust you.
 You enter the shop and they sell a very shiny, beautiful and expensive battle axe.
 But the axe is a bit too expensive.
 What will you do? Ask for a cheaper price (just type 'cheaper') or steal: """,
 
-"12": """You steal the axe and run. The villagers get really mad and out of anger they trap 3 of your men.
+"""You steal the axe and run. The villagers get really mad and out of anger they trap 3 of your men.
 You want to save them but you don't know if you should.
 What will you do? Save of continue: """,
 
-"13": """You know you can't save them, so you just move along.
+"""You know you can't save them, so you just move along.
 You arrive at the city wall. You walk towards it, but the gate is closed.
 The guards don't really trust you yet, and are strict.
 You noticed a barrel full of gunpowder in the village, but you could also just ask.
 What will you do? Blow up the wall (just type 'blow up') or ask: """,
 
-"14": """You ask if it can be made cheaper, but the shopkeeper tells you that the axe is the only one ever made and that they won't make it cheaper.
+"""You ask if it can be made cheaper, but the shopkeeper tells you that the axe is the only one ever made and that they won't make it cheaper.
 You say you understand and you move on.
 Eventually you arive at the city wall. But the gate is closed shut.
 You notice a gunpowder barrel in the village, but you can also just ask the guards, but they seem in a bad mood...
 What will you do? Blow the wall up (just type 'blow up') or ask: """,
 
-"15": """You ask the guards if you can get past, but they say no.
+"""You ask the guards if you can get past, but they say no.
 But you really have to get past.
 What will you do? threaten or tell who you sent (just type 'tell'): """,
 
-"16": """The guards accept it and open the gate for you.
+"""The guards accept it and open the gate for you.
 You get on the other side, into a big open area. And out of nowhere some bandits appear.
 They tell you that they want all your stuff. But you won't just give your items to the bandits.
 But you don't know if you can beat them.
 What will you do? Run, fight or surrender: """,
 
-"17": """"You run away, but the badits shoot 5 men with their crossbows straight in the neck.
+""""You run away, but the badits shoot 5 men with their crossbows straight in the neck.
 They don't survive, and you continue the path.
 You walk up a hill and see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
 How can you be let in? Choose ask or threaten: """,
 
-"18": """Because you are all better trained and better equipped, you managed to defeat the bandits.
+"""Because you are all better trained and better equipped, you managed to defeat the bandits.
 You walk up a hill and see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
 How can you be let in? Choose ask or threaten: """,
 
-"19": """You mined the gold. You put it in your bags and continue the journey.
+"""You mined the gold. You put it in your bags and continue the journey.
 You and your team walk to the exit of the cave, and end up on the other side.
 You see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
 How can you be let in? Choose ask or threaten: """,
 
-"20": """You mined the gold. You put it in your bags and continue the journey.
+"""You mined the gold. You put it in your bags and continue the journey.
 You and your team walk to the exit of the cave, and end up on the other side.
 You see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
 How can you be let in? Choose ask or threaten: """,
 
-"21": """You didn't mine the gold.
+"""You didn't mine the gold.
 You and your team walk to the exit of the cave, and end up on the other side.
 You see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
 How can you be let in? Choose ask or threaten: """,
 
-"22": """You have nothing to mine the gold with.
+"""You have nothing to mine the gold with.
 You and your team walk to the exit of the cave, and end up on the other side.
 You see the enemy castle in the distance. Waving the letter around, you walk towards it.
 But at the gate stand some guards, who are not happy.
