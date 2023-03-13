d7cdf3adf618fdca4e67f227ac107193785c0bab
Seantheman01 <sean.v.g@outlook.com>
2023-03-13 13:05:49 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -2,7 +2,8 @@ from game_storyline import storyline
 
 soldaten = 20
 geld = 100
-naam = input("Before we start, what is your name? ")
+naam = input("""---------- Welcome to Journey! ----------
+Before we start, what is your name? """)
 intro = input(f"""
 Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. One of the first paths is harder than the other.
 That is all you need to know for now. 
