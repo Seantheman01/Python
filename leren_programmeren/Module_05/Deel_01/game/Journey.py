7677ca1b4b2c3a248e87847690e122550d5ffe89
Seantheman01 <sean.v.g@outlook.com>
2023-03-13 14:04:16 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -1,24 +1,23 @@
 from game_storyline import storyline
 from game_plaatjes import plaatjes
 
-print(plaatjes[1])
-# soldaten = 20
-# geld = 100
-# naam = input("""---------- Welcome to Journey! ----------
-# Before we start, what is your name? """)
-# intro = input(f""" Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
-# One of the first paths is harder than the other.
-# That is all you need to know for now. 
-# Good luck on your journey! Are you ready? (just type 'yes or no') """)
-# if intro == 'no':
-#     print("Then why are you playing this?")
-# elif intro == 'yes':
-#     print(plaatjes[0])
-#     begin = input(storyline[0])
-# else:
-#     begin = input("Choose forest or village: ")
+soldaten = 20
+geld = 100
+naam = input("""---------- Welcome to Journey! ----------
+Before we start, what is your name? """)
+intro = input(f""" Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
+One of the first paths is harder than the other.
+That is all you need to know for now. 
+Good luck on your journey! Are you ready? (just type 'yes or no') """)
+if intro == 'no':
+    print("Then why are you playing this?")
+elif intro == 'yes':
+    print(plaatjes[0])
+    begin = input(storyline[0])
+else:
+    begin = input("Choose forest or village: ")
 
-#     if storyline[0] == 'forest':
-#         print(plaatjes[1])
-#         pad1 = input(storyline[1])
+    if storyline[0] == 'forest':
+        print(plaatjes[1])
+        pad1 = input(storyline[1])
     
