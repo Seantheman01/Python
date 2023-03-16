e0c25b4317c11da6c1721b9dacc8edff879c0831
Seantheman01 <sean.v.g@outlook.com>
2023-03-16 11:33:21 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -17,28 +17,30 @@ if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
     n2 = int(input("Voer het tweede getal in: "))
     
     if choice == 'a':
-        n1 + n2
+        uitkomst = n1 + n2
     elif choice == 'b':
-        n1 - n2
+        uitkomst = n1 - n2
     elif choice == 'c':
-        n1 * n2
+        uitkomst = n1 * n2
     elif choice == 'd':
-        n1 / n2
+        uitkomst = n1 / n2
 
 elif choice == 'e' or choice == 'f':
     n1 = int(input("Voer een getal in: "))
     n2 = 1
     
     if choice == 'e':
-        n1 + 1
+        uitkomst = n1 + 1
     elif choice == 'f':
-        n1 - 1
+        uitkomst = n1 - 1
     
 elif choice == 'g' or choice == 'h':
     n1 = int(input("Voer een getal in: "))
     n2 = 2
 
     if choice == 'g':
-        n1 * 2
+        uitkomst = n1 * 2
     elif choice == 'h':
-        n1 / 2
+        uitkomst = n1 / 2
+        
+choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?")
