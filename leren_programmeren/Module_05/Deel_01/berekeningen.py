b9120c4aa542553d300bfd39a2efb87d02c972ad
Seantheman01 <sean.v.g@outlook.com>
2023-03-17 09:28:44 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -9,7 +9,7 @@ def multiplication(number1, number2):
     
 def division(number1, number2):
     print(number1 / number2)
-    
+   
 choice = input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
 
 if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
@@ -43,6 +43,7 @@ elif choice == 'g' or choice == 'h':
     elif choice == 'h':
         uitkomst = n1 / 2
         
+print(uitkomst)        
 choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets? ")
 if choice != 'i':
     uitkomst = n1
