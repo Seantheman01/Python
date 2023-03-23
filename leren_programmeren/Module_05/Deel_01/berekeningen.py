45e96f4b052e4b07b6746f6c30309f35714a1e1a
Seantheman01 <sean.v.g@outlook.com>
2023-03-23 13:29:44 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -12,11 +12,11 @@ def division(number1, number2):
    
 choice = input("Wat wilt je doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
 
+if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
+    n1 = int(input("Voer het eerste getal in: "))
+    n2 = int(input("Voer het tweede getal in: "))
+
 while True: 
-    if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
-        n1 = int(input("Voer het eerste getal in: "))
-        n2 = int(input("Voer het tweede getal in: "))
-        
         if choice == 'a':
             uitkomst = addition(n1, n2)
             print(f"{n1} + {n2} = {uitkomst}")
@@ -30,31 +30,31 @@ while True:
             uitkomst = division(n1, n2)
             print(f"{n1} : {n2} = {uitkomst}")
             
-    elif choice == 'e' or choice == 'f':
-        n1 = int(input("Voer een getal in: "))
-        n2 = 1
-        
-        if choice == 'e':
-            uitkomst = addition(n1, n2)
-            print(f"{n1} + {n2} = {uitkomst}")
-        elif choice == 'f':
-            uitkomst = substraction(n1, n2)
-            print(f"{n1} - {n2} = {uitkomst}")
-        
-    elif choice == 'g' or choice == 'h':
-        n1 = int(input("Voer een getal in: "))
-        n2 = 2
+        elif choice == 'e' or choice == 'f':
+            n1 = int(input("Voer een getal in: "))
+            n2 = 1
+            
+            if choice == 'e':
+                uitkomst = addition(n1, n2)
+                print(f"{n1} + {n2} = {uitkomst}")
+            elif choice == 'f':
+                uitkomst = substraction(n1, n2)
+                print(f"{n1} - {n2} = {uitkomst}")
+            
+        elif choice == 'g' or choice == 'h':
+            n1 = int(input("Voer een getal in: "))
+            n2 = 2
 
-        if choice == 'g':
-            uitkomst = multiplication(n1, n2)
-            print(f"{n1} x {n2} = {uitkomst}")
-        elif choice == 'h':
-            uitkomst = division(n1, n2)
-            print(f"{n1} : {n2} = {uitkomst}")
-                
-    choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets? ")
-    n1 = int(input("Voer een getal in: "))
-    n2 = uitkomst
-    
-    if choice == 'i':
-        break
+            if choice == 'g':
+                uitkomst = multiplication(n1, n2)
+                print(f"{n1} x {n2} = {uitkomst}")
+            elif choice == 'h':
+                uitkomst = division(n1, n2)
+                print(f"{n1} : {n2} = {uitkomst}")
+                    
+        choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets? ")
+        n1 = int(input("Voer een getal in: "))
+        n2 = uitkomst
+        
+        if choice == 'i':
+            break
