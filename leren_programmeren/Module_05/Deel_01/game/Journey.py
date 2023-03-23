a1d5e6e31bc9293a86475025a7ea7eb4082ee129
Seantheman01 <sean.v.g@outlook.com>
2023-03-23 15:48:12 +0100

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -5,17 +5,20 @@ soldaten = 20
 geld = 100
 naam = input("""---------- Welcome to Journey! ----------
 Before we start, what is your name? """)
-intro = input(f""" Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
+INTRO = input(f""" Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
 One of the first paths is harder than the other.
 That is all you need to know for now. 
 Good luck on your journey! Are you ready? (just type 'yes or no') """)
-if intro == 'no':
+if INTRO == 'no':
     print("Then why are you playing this?")
-elif intro == 'yes':
-    print(plaatjes[0])
-    BEGIN = input(storyline[0])
-    if BEGIN == 'forest':
-        print(plaatjes[1])
-        pad1_vraag = input(storyline[1])
+elif INTRO == 'yes':
+    while True:
+        print(plaatjes[0])
+        BEGIN = input(storyline[0])
+        if BEGIN == 'forest':
+            print(plaatjes[1])
+            pad1_vraag = input(storyline[1])
+            if pad1_vraag == 'elf village':
+                SPULLEN_VRAAG = input(storyline[2])
 else:
-    begin = input("Choose yes or no")
+    BEGIN = input("Choose yes or no")
