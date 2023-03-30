c7f5b338f4079a43b776b56919fccf16b17a39bb
Seantheman01 <sean.v.g@outlook.com>
2023-03-30 14:43:59 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -1,19 +1,21 @@
 from game_storyline import storyline
 from game_plaatjes import plaatjes
+from game_GAME_OVER import GAME_OVER
 
-soldaten = 20
-geld = 100
-naam = input("""---------- Welcome to Journey! ----------
-Before we start, what is your name? """)
-INTRO = input(f"""Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
-One of the first paths is harder than the other.
-That is all you need to know for now. 
-Good luck on your journey! Are you ready? (just type 'yes or no') """)
-if INTRO == 'no':
-    print("Then why are you playing this?")
-elif INTRO == 'yes':
-    while True:
-        print(plaatjes[0])
-        BEGIN = input(storyline[0])              
-else:
-    BEGIN = ("Choose yes or no")
+print(GAME_OVER[0])
+# soldaten = 20
+# geld = 100
+# naam = input("""---------- Welcome to Journey! ----------
+# Before we start, what is your name? """)
+# INTRO = input(f"""Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
+# One of the first paths is harder than the other.
+# That is all you need to know for now. 
+# Good luck on your journey! Are you ready? (just type 'yes or no') """)
+# if INTRO == 'no':
+#     print("Then why are you playing this?")
+# elif INTRO == 'yes':
+#     while True:
+#         print(plaatjes[0])
+#         BEGIN = input(storyline[0])              
+# else:
+#     BEGIN = ("Choose yes or no")
