f85382432fb8fa99996573277599b62500f4e8bf
Seantheman01 <sean.v.g@outlook.com>
2023-04-05 13:55:07 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -24,12 +24,16 @@ elif INTRO == 'yes':
                 if ELF_DORP == 'buy':
                     PRODUCTEN = input(storyline[3])
                     if PRODUCTEN == 'sword':
-                        KOPEN = input(f"""You bought the {PRODUCTEN} for 30 coins from the shop, but you realize the rest of your team can't afford anything.
+                        print(plaatjes[2])
+                        PAD2 = input(f"""You bought the {PRODUCTEN} for 30 coins from the shop, but you realize the rest of your team can't afford anything.
 Eventually you reach the lake, but you need to get across.
 How will you cross the river: swim or use a log (just type 'log')? """)
-                        if KOPEN == 'log':
-                            PAD2 = input(storyline[6])
-                            
-
+                        if PAD2 == 'log':
+                            print(plaatjes[3])
+                            GROT = input(storyline[6])
+                            if GROT == 'mine':
+                               TROL = input(storyline[7])
+                                
+                                
 else:
     BEGIN = input("Choose yes or no: ")
