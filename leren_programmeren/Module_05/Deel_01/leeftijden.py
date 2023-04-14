b6ad6f75a36b4eccd1e55575415ef9ae802ebf13
Seantheman01 <sean.v.g@outlook.com>
2023-04-14 10:02:53 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/leeftijden.py
+++ leren_programmeren/Module_05/Deel_01/leeftijden.py
@@ -5,11 +5,11 @@ def mijn_functie():
         namen_en_leeftijden['naam'] = naam
         leeftijd = int(input("Typ een leeftijd in: "))
         namen_en_leeftijden['leeftijd'] = leeftijd
-        
+  
         return namen_en_leeftijden
-    
+
         # verder = input("Toets enter om door te gaan of 'stop' om te printen: ")
         # if verder == 'stop':
         #     break
-            
+
 print(mijn_functie())
