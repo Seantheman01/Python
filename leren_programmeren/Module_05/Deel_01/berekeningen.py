1b1053a63be9ce31c3cfa02a768b72f7e0e2d5fb
Seantheman01 <sean.v.g@outlook.com>
2023-04-17 09:21:35 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/berekeningen.py
+++ leren_programmeren/Module_05/Deel_01/berekeningen.py
@@ -1,18 +1,18 @@
 def addition(number1, number2):
     return number1 + number2
-    
+
 def substraction(number1, number2):
     return number1 - number2
-    
+
 def multiplication(number1, number2):
     return number1 * number2
-    
+
 def division(number1, number2):
     return number1 / number2
-   
+
 choice = input("Wat wilt je doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren? ")
 n1 = int(input("Voer het eerste getal in: "))
-    
+
 while True:
     if choice == 'a':
         n2 = int(input("Voer het tweede getal in: "))
@@ -30,7 +30,7 @@ while True:
         n2 = int(input("Voer het tweede getal in: "))
         uitkomst = division(n1, n2)
         print(f"{n1} : {n2} = {uitkomst}")
-        
+
     if choice == 'e':
         n2 = 1
         uitkomst = addition(n1, n2)
@@ -48,7 +48,7 @@ while True:
         n2 = 2
         uitkomst = division(n1, n2)
         print(f"{n1} : {n2} = {uitkomst}")
-                
+    
     choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets? ")
     if choice == 'a' or choice == 'b' or choice == 'c' or choice == 'd':
         n1 = uitkomst
