79c36ccce72b3c820fb67741568d9f1a44da941b
Seantheman01 <sean.v.g@outlook.com>
2023-04-20 10:45:30 +0200

    committed

--- extra/AVI_challenge/unit_tests.py
+++ extra/AVI_challenge/unit_tests.py
@@ -21,16 +21,16 @@ print(getNumberOfSentences(text=str))
 # schrijf zelf nog een extra testen voor getNumberOfSentences (gebruik test.txt).
 
 # test 3: getNumberOfWords
-# print(getNumberOfWords(getText('data\difficult1.txt')))
-# if getNumberOfWords(getText('data\difficult1.txt')) == 82:
-#     print("Test geslaagd")
-# else:
-#     print("Deze test is niet geslaagd")
+print(getNumberOfWords(getText('data\difficult1.txt')))
+if getNumberOfWords(getText('data\difficult1.txt')) == 82:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
 
-# if getNumberOfWords(getText('data\easy1.txt')) == 11:
-#     print("Test geslaagd")
-# else:
-#     print("Deze test is niet geslaagd")
+if getNumberOfWords(getText('data\easy1.txt')) == 11:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
     
 print(getNumberOfWords(text=str))
 
