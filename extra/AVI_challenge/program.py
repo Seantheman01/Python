e7a0f765c97e7dd15b2e3d4571eaaf5151b541f0
Seantheman01 <sean.v.g@outlook.com>
2023-04-20 13:23:44 +0200

    committed

--- extra/AVI_challenge/program.py
+++ extra/AVI_challenge/program.py
@@ -7,6 +7,7 @@ import tkinter.messagebox # if you want to send messages to the user.
 def calculate():
     textToBeCalculated = calculateInput.get('1.0', 'end') # get all the text from inputfield
     characterLabel.config(text=f"Karakter: {getNumberOfCharacters(textToBeCalculated)}")
+    wordsLabel.config(text=f"Woorden: {getNumberOfWords(textToBeCalculated)}")
     sentencesLabel.config(text =f"Zinnen: {getNumberOfSentences(textToBeCalculated)}")
 
 
@@ -14,14 +15,16 @@ def calculate():
 root = tk.Tk()              # create tkInter window
 root.title('Text analyser') # set title
 root.geometry('600x600')    # set dimension
-calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')            # generate imput element
+calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')       # generate imput element
 calculateButton = ttk.Button(root, text='Bereken score(s)', command=calculate)        # generate button when pressed -> calculate
 characterLabel = tk.Label(root, text=f'Karakters:', width=20, bg='black', fg='white') # generate characterLabel
+wordsLabel = tk.Label(root, text=f'Woorden:', width=20, bg='black', fg='white')       # generate wordsLabel
 sentencesLabel = tk.Label(root, text=f'Zinnen:', width=20, bg='black', fg='white')    # generate sentencesLabel
 
 calculateInput.place(x=20, y=20)   # place is one of the ways to put elements on root (window).
 calculateButton.place(x=20, y=520)
 characterLabel.place(x=180, y=560)
+wordsLabel.place(x=340, y=560)
 sentencesLabel.place(x=20, y=560)
 
 # start program
