ba863afb0466fe9436cdacf02105795ff8e2f50d
Seantheman01 <sean.v.g@outlook.com>
2023-05-11 09:42:58 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -2,8 +2,8 @@ from game_storyline import storyline
 from game_plaatjes import plaatjes
 from game_over import game_over
 
-soldaten = 20
-geld = 100
+soldaten = [20]
+geld = [100]
 naam = input("""---------- Welcome to Journey! ----------
 Before we start, what is your name? """)
 INTRO = input(f"""Hello {naam}! Here are some tips: You have 20 soldiers with you, and you start off with 100 coins. 
@@ -24,10 +24,10 @@ elif INTRO == 'yes':
                 if ELF_DORP == 'buy':
                     SPULLEN = input(storyline[3])
                     if SPULLEN == 'sword' or SPULLEN == 'pickaxe':
+                        print(plaatjes[2])
                         PAD2 = input(f"""You bought the {SPULLEN} for 30 coins from the shop, but you realize the rest of your team can't afford anything.
 Eventually you reach the lake, but you need to get across.
 How will you cross the river: swim or use a log (just type 'log')? """)
-                        print(plaatjes[2])
                         if PAD2 == 'log':
                             print(plaatjes[3])
                             GROT = input(storyline[6])
