e15f150236b13fcc1d3cc1e3323becb047521b7a
Seantheman01 <sean.v.g@outlook.com>
2023-05-26 09:29:23 +0200

    committed

--- leren_programmeren/Module_05/Deel_02/Treausure_Hunt/functions.py
+++ leren_programmeren/Module_05/Deel_02/Treausure_Hunt/functions.py
@@ -5,16 +5,16 @@ from data import JOURNEY_IN_DAYS
 ##################### M04.D02.O2 #####################
 
 def copper2silver(amount:int) -> float:
-    pass
+    return amount / 10
 
 def silver2gold(amount:int) -> float:
-    pass
+    return amount / 5
 
 def copper2gold(amount:int) -> float:
-    pass
+    return amount / 50
 
 def platinum2gold(amount:int) -> float:
-    pass
+    return amount * 25
 
 def getPersonCashInGold(personCash:dict) -> float:
     pass
