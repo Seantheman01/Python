f1ce4379228168c5d95128c436f527d013228bb1
Seantheman01 <sean.v.g@outlook.com>
2023-06-02 13:23:34 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/game_new/game_over.py
+++ leren_programmeren/Module_05/Deel_01/game_new/game_over.py
@@ -44,37 +44,5 @@ game_over = [
     \_  \          /,L]     /
       '-_`-,       ` `   ./`
          `-(_            )
-             ^^\..___,.--`""",
-
-"""You and your team get stuck in the mud. You all start sinking slowely.
-GAME OVER""",
-
-"""You all take of your armor and try to swim across.
-But the water is flowing too fast, and you all get taken by the water.
-Swimming also started to get really difficult, so you all drown.
-GAME OVER""",
-
-"""You enter a dark and narrow cave system.
-You all get lost and after walking around witch no succes.
-Suddenly you all fall in a pit.
-GAME OVER""",
-
-"""The troll is way bigger and stronger than you.
-He hits you and you fly backwards.
-You don't surive the attack.
-GAME OVER""",
-
-"""You threaten the guards, and that makes them really mad.
-The guards on top of the towers aim their bow and shoot.
-They hit you right in the head.
-GAME OVER""",
-
-"""The bandits tie everyone up and steal your supplies.
-Evryone is left behind, laying in the grass.
-GAME OVER""",
-
-"""You threaten the guards, and that makes them really mad.
-The guards on top of the towers aim their crossbows and shoot.
-They hit you right in the neck.
-GAME OVER"""
+             ^^\..___,.--`"""
 ]
