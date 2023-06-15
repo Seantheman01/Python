35d966941e44327361b0cbeaf10201cb7281ee0f
Seantheman01 <sean.v.g@outlook.com>
2023-06-15 09:51:20 +0200

    committed

--- leren_programmeren/Module_05/Deel_01/game_new/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game_new/Journey.py
@@ -10,30 +10,32 @@ vijanden = [{
     'appearence' : plaatjes[8]
 }]  
 
-naam = input("""---------- Welcome to Journey! ----------
-Before we start, what is your name? """)
-intro = input(f"""
-Hello {naam}! Here are some tips: you start off with 100 coins,
-and one of the first paths is harder than the other.
-That is all you need to know for now. 
-Good luck on your journey! Are you ready? (just type 'yes or no') """)
-if intro == 'no':
-    print("Then why are you playing this?")
-elif intro == 'yes':
-    print(plaatjes[0])
-    begin = input(storyline[0]) 
-    if begin == 'forest':
-        print(plaatjes[1])
-        elf_winkel = input(storyline[1])
-        if elf_winkel == 'sword':
-            print(plaatjes[2])
-            print("""You bought the sword from the shop. 
-Your attack increased!""")
-        elif elf_winkel == 'shield':
-            print(plaatjes[3])
-            print("""You bought the shield from the shop. 
-Your defense increased!""")
-        elif elf_winkel == 'none':
-            print("You bought nothing from the shop.")
+# naam = input("""---------- Welcome to Journey! ----------
+# Before we start, what is your name? """)
+# intro = input(f"""
+# Hello {naam}! Here are some tips: you start off with 100 coins,
+# and one of the first paths is harder than the other.
+# That is all you need to know for now. 
+# Good luck on your journey! Are you ready? (just type 'yes or no') """)
+# if intro == 'no':
+#     print("Then why are you playing this?")
+# elif intro == 'yes':
+#     print(plaatjes[0])
+#     begin = input(storyline[0]) 
+#     if begin == 'forest':
+#         print(plaatjes[1])
+#         elf_winkel = input(storyline[1])
+#         if elf_winkel == 'sword':
+#             print(plaatjes[2])
+#             print("""You bought the sword from the shop. 
+# Your attack increased!""")
+#         elif elf_winkel == 'shield':
+#             print(plaatjes[3])
+#             print("""You bought the shield from the shop. 
+# Your defense increased!""")
+#         elif elf_winkel == 'none':
+#             print("You bought nothing from the shop.")
 
-print(storyline[2])
+# print(storyline[2])
+
+print(f"A {vijanden[0]} appears")
