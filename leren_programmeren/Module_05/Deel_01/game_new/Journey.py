012a8ad9bbb854d0892c1a0d656a5237baf4214b
Sean <sean@avant.nl>
2023-12-12 14:11:24 +0100

    .

--- leren_programmeren/Module_05/Deel_01/game_new/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game_new/Journey.py
@@ -2,12 +2,18 @@ from game_storyline import storyline
 from game_plaatjes import plaatjes
 from game_over import game_over
 
+jouw_aanvallen = """What will you do? :
+Attack
+Block
+Heal
+"""
+
 vijanden = {
     'creature' : 'goblin',
     'health' : 50,
-    'attack' : 'club hit',
+    'attack' : 'stab',
     'damage' : 20
-}  
+}
 
 naam = input("""---------- Welcome to Journey! ----------
 Before we start, what is your name? """)
@@ -16,9 +22,7 @@ Hello {naam}! Here are some tips: you start off with 100 coins,
 and one of the first paths is harder than the other.
 That is all you need to know for now. 
 Good luck on your journey! Are you ready? (just type 'yes or no') """)
-if intro == 'no':
-    print("Then why are you playing this?")
-elif intro == 'yes':
+if intro == 'yes':
     print(plaatjes[0])
     begin = input(storyline[0]) 
     if begin == 'forest':
@@ -35,8 +39,12 @@ Your defense increased!""")
         elif elf_winkel == 'none':
             print("You bought nothing from the shop.")
 
-print(storyline[2])
+        print(storyline[2])
 
-print(""" 
-A """ + vijanden["creature"] + " appears")
-print(plaatjes[8])
+        print("Suddenly, a " + vijanden["creature"] + " appears")
+        print(plaatjes[8])
+
+        input(jouw_aanvallen)
+
+elif intro == 'no':
+    print("Then why are you playing this?")
--- leren_programmeren/Module_05/Deel_01/game_new/game_storyline.py
+++ leren_programmeren/Module_05/Deel_01/game_new/game_storyline.py
@@ -4,7 +4,7 @@ He sends you and a few other knights out with a letter to end the war.
 Starting your journey, you have to choose 2 paths first: forest (harder route) or village (easier route).
 Choose wich path to go: """,
 
-"""You and your team chose the forest. You enter the elf village and enter the shop.
+"""You and your team chose the forest. You head into the elf village and enter the shop.
 There are 2 items for sale: a sword (40 coins) or a shield (30 coins). Wich item do you want to buy? (type none if you don't want to buy anything.) """,
 
 """
