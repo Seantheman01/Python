2c46d8bb8216e88a72f763b8b4d1838d299bfa50
Seantheman01 <sean.v.g@outlook.com>
2024-07-21 14:47:58 +0200

    intro added

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -2,3 +2,5 @@ from game_storyline import storyline
 from game_plaatjes import plaatjes
 from game_over import game_over
 
+print(plaatjes[0])
+print(storyline[0])
--- leren_programmeren/Module_05/Deel_01/game/game_storyline.py
+++ leren_programmeren/Module_05/Deel_01/game/game_storyline.py
@@ -2,7 +2,7 @@ storyline = [
 """You are the best trained commander of an army. You have been at war with your enemy for many years, and your king is done with it. 
 He sends you on your own, because they can't risk loosing more people. 
 You are send out with a letter to end the war.
-Starting your journey, you have to choose 2 paths first: forest (harder route) or village (easier route).
+Starting your journey, you have to choose 2 paths first: village (easier route) or forest (harder route).
 Choose wich path to go: """,
 
 """
