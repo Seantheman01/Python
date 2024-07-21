062c384ab3cc79979917907218a3d5998fdbc0af
Seantheman01 <sean.v.g@outlook.com>
2024-07-21 15:35:19 +0200

    dictionaries added

--- leren_programmeren/Module_05/Deel_01/game/Journey.py
+++ leren_programmeren/Module_05/Deel_01/game/Journey.py
@@ -2,5 +2,28 @@ from game_storyline import storyline
 from game_plaatjes import plaatjes
 from game_over import game_over
 
+invertory = {
+    "Bag of money": {
+        "amount": 200
+    },
+   
+    "Steel sword": {
+        "cost": 20,
+        "damage": 15 
+    }
+}
+
+wapens = {
+    "Crystal sword": {
+        
+    }
+}
+
 print(plaatjes[0])
 print(storyline[0])
+pad_1 = input(storyline[1])
+
+if pad_1 == "village" or "Village" or "VILLAGE":
+    naar_binnen = input(storyline[2])
+    if naar_binnen == "yes":
+        pass
--- leren_programmeren/Module_05/Deel_01/game/game_storyline.py
+++ leren_programmeren/Module_05/Deel_01/game/game_storyline.py
@@ -1,20 +1,16 @@
 storyline = [
 """You are the best trained commander of an army. You have been at war with your enemy for many years, and your king is done with it. 
 He sends you on your own, because they can't risk loosing more people. 
-You are send out with a letter to end the war.
+You are send out with a letter to end the war.""",
+
+"""
 Starting your journey, you have to choose 2 paths first: village (easier route) or forest (harder route).
 Choose wich path to go: """,
 
 """
-You chose the forest. You follow the path, until all of a sudden...""",
-
-"""
-You kept following the path until you came across the elf village.
-You head into the elf village and enter the shop.
-There are 2 items for sale: a sword (40 coins) or a shield (30 coins). Wich item do you want to buy? (type 'none' if you don't want to buy anything.) """,
-
-"""
-You continue your journey and you come across a river with a waterfall.
-But it seems that the bridge is broken.
-But then, out of nowhere..."""
+You chose the village.
+When you arrive, the villagers seem to be looking at you with suspicion.
+But you just decide to ignore it.
+You come across a shop, wich seems to selling some weapons. 
+Will you enter the shop? """
 ]
