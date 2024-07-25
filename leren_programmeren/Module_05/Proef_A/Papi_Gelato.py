f351e36f1c9d11c63668020519ca1d01ccb958d4
Seantheman01 <sean.v.g@outlook.com>
2024-07-25 23:31:14 +0200

    fix

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -1,3 +1,3 @@
-print("Welkom bijPapi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
+print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
 int(input("Hoeveel bolletjes wilt u?"))
