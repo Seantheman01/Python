e86b2633850c2afe3ae7d4aa509aa44894993083
Seantheman01 <sean.v.g@outlook.com>
2024-07-26 17:16:04 +0200

    new input

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -2,4 +2,4 @@ print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille
 
 aantal = int(input("Hoeveel bolletjes wilt u? "))
 if aantal == 1 or aantal == 2 or aantal == 3:
-    print("Het werkt!")
+    soort = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje?")
