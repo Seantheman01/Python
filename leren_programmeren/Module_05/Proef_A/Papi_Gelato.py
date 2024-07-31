18fa0ae4b3739b5d174b320c3c9d3b323952a263
Seantheman01 <sean.v.g@outlook.com>
2024-07-31 18:54:57 +0200

    first step added

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -1,6 +1,16 @@
 print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
 def aantal_bolletjes():
-    aantal = int(input("Hoeveel bolletjes wilt u? "))
-    if aantal == 1 or aantal == 2 or aantal == 3:
-        pass
+    while True:
+        aantal = int(input("Hoeveel bolletjes wilt u? "))
+        if aantal >= 1 and aantal <= 3:
+            break
+        elif aantal >= 4 and aantal <= 8:
+            print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
+            break
+        elif aantal > 8:
+            print('Sorry, zulke grote bakken hebben we niet.')
+        else:
+            print("Sorry dat snap ik niet...")
+
+aantal_bolletjes()
