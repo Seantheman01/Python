c100ff6af3d3576ff8a47ddcd493951fb1d5e3ab
Seantheman01 <sean.v.g@outlook.com>
2024-07-31 19:19:28 +0200

    trying to fix some stuff..........................

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -1,26 +1,18 @@
 print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
 def aantal_bolletjes():
-    aantal = int(input("Hoeveel bolletjes wilt u? "))
-    if aantal >= 1 and aantal <= 3:
-        pass
-    elif aantal >= 4 and aantal <= 8:
-        print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
-    elif aantal > 8:
-        print('Sorry, zulke grote bakken hebben we niet.')
-    else:
-        print("Sorry dat snap ik niet...")
+    while True:
+        aantal = int(input("Hoeveel bolletjes wilt u? "))
+        if aantal >= 1 and aantal <= 3:
+            break
+        elif aantal >= 4 and aantal <= 8:
+            print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
+            break
+        elif aantal > 8:
+            print('Sorry, zulke grote bakken hebben we niet.')
+        else:
+            print("Sorry dat snap ik niet...")
             
-# return aantal_bolletjes
+        # return aantal_bolletjes
 
 aantal = aantal_bolletjes()
-
-
-def hoorn_of_bak():
-    soort = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ")
-    if soort == "hoorntje" or soort == "bakje":
-        pass
-        
-# return hoorn_of_bak
-
-hoorn_of_bak()
