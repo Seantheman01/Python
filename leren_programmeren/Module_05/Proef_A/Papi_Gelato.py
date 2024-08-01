e40da0e669f9eae4f640fbdebb5e1fcb3c412780
Seantheman01 <sean.v.g@outlook.com>
2024-08-01 16:38:26 +0200

    removed unused stuff

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -1,48 +1,43 @@
 print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
-def aantal_bolletjes():
-    while True:
-        aantal = int(input("Hoeveel bolletjes wilt u? "))
-        if aantal >= 1 and aantal <= 3:
-            break
-        elif aantal >= 4 and aantal <= 8:
-            print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
-            break
-        elif aantal > 8:
-            print('Sorry, zulke grote bakken hebben we niet.')
-        else:
-            print("Sorry dat snap ik niet...")
-            
-        return aantal_bolletjes
+while True:
+    def aantal_bolletjes():
+        while True:
+            aantal = int(input("Hoeveel bolletjes wilt u? "))
+            if aantal >= 1 and aantal <= 3:
+                break
+            elif aantal >= 4 and aantal <= 8:
+                print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
+                break
+            elif aantal > 8:
+                print('Sorry, zulke grote bakken hebben we niet.')
+            else:
+                print("Sorry dat snap ik niet...")
 
-aantal = aantal_bolletjes()
-
-
-def hoorn_of_bak():
-    while True:
-        soort = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ")
-        if soort == "hoorntje" or soort == "bakje":
-            break
-        else:
-            print("Sorry dat snap ik niet...")
-            
-    return hoorn_of_bak
-
-soort = hoorn_of_bak()
+    aantal = aantal_bolletjes()
 
 
-def meer_bestellen():
-    while True:
-        print(f"Hier is uw {soort} met {aantal} bolletje(s).")
-        opnieuw = input("Wilt u nog meer bestellen?")
-        if opnieuw == "ja":
-            break
-        elif opnieuw == "nee":
-            print("Bedankt en tot ziens!")
-            break
-        else:
-            print("Sorry dat snap ik niet...")
-            
-        return meer_bestellen
+    def hoorn_of_bak():
+        while True:
+            soort = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ")
+            if soort == "hoorntje" or soort == "bakje":
+                break
+            else:
+                print("Sorry dat snap ik niet...")
 
-opnieuw = meer_bestellen()
+    soort = hoorn_of_bak()
+
+
+    def meer_bestellen():
+        while True:
+            print(f"Hier is uw {soort} met {aantal} bolletje(s).")
+            opnieuw = input("Wilt u nog meer bestellen? ")
+            if opnieuw == "ja":
+                break
+            elif opnieuw == "nee":
+                print("Bedankt en tot ziens!")
+                break
+            else:
+                print("Sorry dat snap ik niet...")
+
+    opnieuw = meer_bestellen()
