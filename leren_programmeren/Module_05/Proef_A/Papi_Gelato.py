b6b65112a893c5017e8d439aa2df8438f5558c2b
Seantheman01 <sean.v.g@outlook.com>
2024-08-13 14:37:21 +0200

    code kinda works

--- leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
+++ leren_programmeren/Module_05/Proef_A/Papi_Gelato.py
@@ -5,10 +5,10 @@ while True:
         while True:
             aantal = int(input("Hoeveel bolletjes wilt u? "))
             if aantal >= 1 and aantal <= 3:
-                break
+                return aantal
             elif aantal >= 4 and aantal <= 8:
                 print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes.')
-                break
+                return aantal
             elif aantal > 8:
                 print('Sorry, zulke grote bakken hebben we niet.')
             else:
@@ -17,27 +17,27 @@ while True:
     aantal = aantal_bolletjes()
 
 
-    def hoorn_of_bak():
+    def hoorn_of_bak(aantal):
         while True:
             soort = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ")
             if soort == "hoorntje" or soort == "bakje":
-                break
+                return soort
             else:
                 print("Sorry dat snap ik niet...")
 
-    soort = hoorn_of_bak()
+    soort = hoorn_of_bak(aantal)
 
 
-    def meer_bestellen():
+    def meer_bestellen(soort, aantal):
         while True:
             print(f"Hier is uw {soort} met {aantal} bolletje(s).")
             opnieuw = input("Wilt u nog meer bestellen? ")
             if opnieuw == "ja":
-                break
+                return opnieuw
             elif opnieuw == "nee":
                 print("Bedankt en tot ziens!")
                 break
             else:
                 print("Sorry dat snap ik niet...")
 
-    opnieuw = meer_bestellen()
+    opnieuw = meer_bestellen(soort, aantal)
